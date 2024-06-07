c40a940983de7b55bf0ed47cc32b808d694ee41f
Cry-StalL <940755868@qq.com>
2024-06-07 18:05:27 +0800

    add currency list and json parser

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
-use currency_converter::get_exchange_rate;
+mod lib;
+use lib::get_exchange_rate;
+use crate::utils::data_parser::get_currency_list;
+
+mod utils;
 
 fn main() {
-    let price: f64 = get_exchange_rate("USD", "CNY").unwrap();
-    println!("{}", price);
+    // let price: f64 = get_exchange_rate("USD", "CNY").unwrap();
+    // println!("{}", price);
+    let list = get_currency_list();
 }
