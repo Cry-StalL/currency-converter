f30bfaa425669e983638d538ac63a88a8e9103dc
Cry-StalL <940755868@qq.com>
2024-06-21 15:47:01 +0800

    put frontend and backend together (but the backend doesn't work due to incompatibility)

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,17 @@
-use reqwest;
-use tokio;
-use serde_json::{Value};
+use yew::prelude::*;
+use stylist::css;
 
-#[tokio::main]
-pub async fn get_exchange_rate(currency_from: &str, currency_to: &str) -> Result<f64, Box<dyn std::error::Error>>{
-    let url = format!("https://finance.pae.baidu.com/vapi/v1/getquotation?group=huilv_minute&need_reverse_real=1&code={currency_from}{currency_to}&finClientType=pc");
-
-    let response = reqwest::get(url).await?;
-    let content = response.text().await?;
-    let content_json: Value = serde_json::from_str(&content)?;
-
-    let exchange_rate: f64 = content_json["Result"]["cur"]["price"].as_str().unwrap().replace("\"", "").parse::<f64>().unwrap();
-
-    Ok(exchange_rate)
+#[function_component]
+pub fn App() -> Html {
+    let style = css!("color: red; font-size: 20px;");
+    html! {
+        <>
+            <div class={style}>
+                {"Hello World!"}
+            </div>
+            <div>
+                <button type="button">{"Click me!"}</button>
+            </div>
+        </>
+    }
 }
